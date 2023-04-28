5d3282de917d3976159cb4e796b88233d2f31da6
naveen kala <kalanaveen55@gmail.com>
2023-04-28 10:10:10 +0530

    home page add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { logo } from '../assets';
 
 const Hero = () => {
   return (
-    <div>
-      hero
-    </div>
-  )
-}
+    <header
+      className="w-full flex justify-center items-center flex-col
+    "
+    >
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button
+          className="black_btn"
+          type="button"
+          onClick={() =>
+            window.open('https://github.com/kalanaveen/aiarticle__summarizer')
+          }
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2>
+        Simplify your reading with summarize,an open source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
